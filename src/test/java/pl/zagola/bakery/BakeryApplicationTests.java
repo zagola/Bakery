39a131d9291be368a0bcdce660630678d5474d2b
zagola <zagola@github.com>
2026-02-25 14:35:16 +0100

    Initialize Spring Boot project with Gradle

---
+++ src/test/java/pl/zagola/bakery/BakeryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zagola.bakery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
