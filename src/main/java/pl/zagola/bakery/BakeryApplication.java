785f6bfa77d52439612402abc26819e9e76e8618
zagola <ozagrabska@gmail.com>
2026-03-11 19:32:04 +0100

    Feature: implementation of methods from PersonDetailsRepository + tests in main() findById() - unique ID search + filter with null safety findByLastName() - search by last name updateLastName() - last name update + filter with null safety main() tests: addPerson, findByLastName, findById, updateLastName, findAll (to check before/after)

--- src/main/java/pl/zagola/bakery/BakeryApplication.java
+++ src/main/java/pl/zagola/bakery/BakeryApplication.java
@@ -12,18 +12,33 @@ public class BakeryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BakeryApplication.class, args);
-        PersonDetails detailsEmployeeJan = new PersonDetails("Jan", "Zieliński");
+        PersonDetails detailsEmployeeJan = new PersonDetails(10000L, "Jan", "Zieliński");
         Employee jan = new Employee(detailsEmployeeJan, Instant.now());
 
-        PersonDetails detailsClientKarol = new PersonDetails("Karol", "Nowak");
+        PersonDetails detailsClientKarol = new PersonDetails(11111L, "Karol", "Nowak");
         Client karol = new Client(detailsClientKarol, new GeographicAddress(30.0, 24.5), 1);
 
         PersonDetailsRepository personDetailsRepository = new PersonDetailsRepositoryListBased();
-        personDetailsRepository.addPerson("Ola", "Zagrabska");
+        personDetailsRepository.addPerson(1L, "Ola", "Zagrabska");
+        personDetailsRepository.addPerson(2L, "Ania", "Wójcik");
+        personDetailsRepository.addPerson(3L, "Jan", "Nowak");
+        personDetailsRepository.addPerson(4L, "Krzysztof", "Kowalski");
+        personDetailsRepository.addPerson(5L, "Tomasz", "Kowalski");
         List<PersonDetails> personDetailsRepositoryAll = personDetailsRepository.findAll();
         System.out.println(personDetailsRepositoryAll);
 
 
+        List<PersonDetails> personDetailsListFindByLastName = personDetailsRepository.findByLastName("Kowalski");
+        System.out.println(personDetailsListFindByLastName);
+
+
+        List<PersonDetails> personToUpdate = personDetailsRepository.findById(5L);
+        System.out.println("Przed zmianą: " + personToUpdate);
+
+        boolean updatedPerson = personDetailsRepository.updateLastName(5L, "NowyKowalski");
+        System.out.println("Zaktualizowano: " + updatedPerson);
+        System.out.println("Po: " + personDetailsRepository.findById(5L));
+        System.out.println(personDetailsRepositoryAll);
     }
 
 }
--- src/main/java/pl/zagola/bakery/PersonDetails.java
+++ src/main/java/pl/zagola/bakery/PersonDetails.java
@@ -1,25 +1,45 @@
 package pl.zagola.bakery;
 
 class PersonDetails {
+    private Long id;
     private String firstName;
     private String lastName;
 
 
-    PersonDetails(String firstName, String lastName) {
+    PersonDetails(Long id, String firstName, String lastName) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+
     }
 
-    public String getLastName() {
-        return lastName;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
     @Override
     public String toString() {
-        return firstName + " " + lastName;
+        return firstName + " " + lastName + " " + id;
     }
 }
