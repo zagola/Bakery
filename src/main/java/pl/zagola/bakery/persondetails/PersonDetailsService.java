017cfcffc159c562f8b190e0633a755889173587
zagola <zagola@github.com>
2026-04-15 09:38:30 +0200

    Feat(bakery): add PersonDetailsService

---
+++ src/main/java/pl/zagola/bakery/persondetails/PersonDetailsService.java
@@ -0,0 +1,23 @@
+package pl.zagola.bakery.persondetails;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonDetailsService {
+    private final PersonDetailsRepository personDetailsRepository;
+
+    public PersonDetailsService(PersonDetailsRepository personDetailsRepository) {
+        this.personDetailsRepository = personDetailsRepository;
+    }
+
+    public List<PersonDetails> getPersonDetails() {
+        return personDetailsRepository.findAll();
+    }
+
+    public boolean addPersonDetails(PersonDetails personDetails) {
+        return personDetailsRepository.addPerson(personDetails);
+    }
+}
