b0979f7697d975c16e9055a702146dead57a313b
zagola <zagola@github.com>
2026-07-01 17:44:17 +0200

    refactor(domain): replace Role entity with enum

--- src/main/java/pl/zagola/bakery/role/Role.java
+++ src/main/java/pl/zagola/bakery/role/Role.java
@@ -1,15 +1,7 @@
 package pl.zagola.bakery.role;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class Role {
-    private long roleId;
-    private String name;
+public enum Role {
+    CLIENT,
+    EMPLOYEE,
+    OWNER
 }
