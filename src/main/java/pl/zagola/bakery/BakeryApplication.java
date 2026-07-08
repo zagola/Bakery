4710169476eb93daa7e974894c5e0265fd8364bc
zagola <ozagrabska@gmail.com>
2026-07-08 15:43:27 +0200

    refactor(application): simplify Spring Boot application entry point

--- src/main/java/pl/zagola/bakery/BakeryApplication.java
+++ src/main/java/pl/zagola/bakery/BakeryApplication.java
@@ -2,23 +2,12 @@ package pl.zagola.bakery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import pl.zagola.bakery.client.repositories.ClientDataRepository;
-import pl.zagola.bakery.persondetails.*;
 
 @SpringBootApplication
 public class BakeryApplication {
 
-    static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(BakeryApplication.class, args);
-        String[] beanDefinitionNames = context.getBeanDefinitionNames();
-        for (String bean : beanDefinitionNames) {
-            System.out.println(bean);
-        }
-
-        ClientDataRepository clientDataRepository = context.getBean(ClientDataRepository.class);
-        System.out.println(clientDataRepository);
-
+    public static void main(String[] args) {
+        SpringApplication.run(BakeryApplication.class, args);
     }
 
 }
